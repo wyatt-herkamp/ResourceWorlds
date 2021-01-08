a4c44543bf0322c5f5a70c27e747dc5432bdba9c
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-01-07 20:22:30 -0500

    Added regen-on-start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SecondEnd [![Build Status](https://ci.potatocorp.dev/job/SecondEnd/badge/icon)](https://ci.potatocorp.dev/job/SecondEnd/)
+
+Creates a secondary end. That can be customized to be reset every x amount of time.
--- src/main/java/me/kingtux/secondend/SecondEnd.java
+++ src/main/java/me/kingtux/secondend/SecondEnd.java
@@ -45,16 +45,58 @@ public final class SecondEnd extends JavaPlugin implements Runnable {
 
     private void loadPlugin() {
         reloadConfig();
-        task = Bukkit.getScheduler().runTaskTimer(this, this, 0, getConfig().getInt("reset-time") * 20).getTaskId();
+        int time = getConfig().getInt("reset-time") * 20;
+        boolean regenOnStart = getConfig().getBoolean("regen-on-start");
+        if (regenOnStart) {
+            //If it is set to regenOnStart delete it
+            deleteWorld();
+        }
+        if (!doesWorldExist()) {
+            createWorld();
+        }
+
+        task = Bukkit.getScheduler().runTaskTimer(this, this, time, time).getTaskId();
         yamlHandler = new BukkitYamlHandler(new File(getDataFolder(), "lang.yml"));
         EnumConfigLoader.loadLang(yamlHandler, SELang.class, true);
+    }
+
+    public boolean doesWorldExist() {
+        String endName = getConfig().getString("end-name");
+        Optional<MultiverseWorld> first = multiverseCore.getMVWorldManager().getMVWorlds().stream().filter(multiverseWorld -> multiverseWorld.getName().equals(endName)).findFirst();
+        return first.isPresent();
+    }
+
+    public void deleteWorld() {
+        String endName = getConfig().getString("end-name");
+        Optional<MultiverseWorld> first = multiverseCore.getMVWorldManager().getMVWorlds().stream().filter(multiverseWorld -> multiverseWorld.getName().equals(endName)).findFirst();
+        if (first.isPresent()) {
+            MultiverseWorld multiverseWorld = first.get();
+            Bukkit.broadcastMessage(SELang.RESETTING_END_ANNOUNCEMENT.color());
+            List<Player> playerList = Bukkit.getOnlinePlayers().stream().filter(onlinePlayer -> onlinePlayer.getWorld() == multiverseWorld.getCBWorld()).collect(Collectors.toList());
+            for (Player player : playerList) {
+                Location returnWorld = Bukkit.getWorld(getConfig().getString("return-world", "world")).getSpawnLocation();
+                player.teleport(returnWorld);
+
+            }
+            multiverseCore.getMVWorldManager().deleteWorld(endName);
+        }
+    }
+
+    public void createWorld() {
+        String endName = getConfig().getString("end-name");
+
+        String endSeed = getConfig().getString("end-seed");
+        if (endSeed.isEmpty()) {
+            endSeed = String.valueOf(random.nextLong());
+        }
+        multiverseCore.getMVWorldManager().addWorld(endName, World.Environment.THE_END, endSeed, WorldType.NORMAL, true, null);
+
     }
 
     private void closePlugin() {
         Bukkit.getScheduler().cancelTask(task);
         String endName = getConfig().getString("end-name");
 
-        multiverseCore.getMVWorldManager().deleteWorld(endName);
 
     }
 
@@ -77,21 +119,9 @@ public final class SecondEnd extends JavaPlugin implements Runnable {
         String endName = getConfig().getString("end-name");
         Optional<MultiverseWorld> first = multiverseCore.getMVWorldManager().getMVWorlds().stream().filter(multiverseWorld -> multiverseWorld.getName().equals(endName)).findFirst();
         if (first.isPresent()) {
-            MultiverseWorld multiverseWorld = first.get();
-            Bukkit.broadcastMessage(SELang.RESETTING_END_ANNOUNCEMENT.color());
-            List<Player> playerList = Bukkit.getOnlinePlayers().stream().filter(onlinePlayer -> onlinePlayer.getWorld() == multiverseWorld.getCBWorld()).collect(Collectors.toList());
-            for (Player player : playerList) {
-                Location returnWorld = Bukkit.getWorld(getConfig().getString("return-world", "world")).getSpawnLocation();
-                player.teleport(returnWorld);
-
-            }
-            multiverseCore.getMVWorldManager().deleteWorld(endName);
+            deleteWorld();
         }
-        String endSeed = getConfig().getString("end-seed");
-        if (endSeed.isEmpty()) {
-            endSeed = String.valueOf(random.nextLong());
-        }
-        multiverseCore.getMVWorldManager().addWorld(endName, World.Environment.THE_END, endSeed, WorldType.NORMAL, true, null);
+        createWorld();
     }
 
     public MultiverseCore getMultiverseCore() {
